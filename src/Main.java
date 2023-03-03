7b9ba61bbfea42494c8d27e798fdbddddc0feb27
shabem <misagh.shabesstarilar@msg.group>
2023-03-03 13:06:10 +0100

    Lets see How it work....

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //create a DoublyLinkedList object
+        DoublyLinkedList dl_List = new DoublyLinkedList();
+        //Add nodes to the list
+        dl_List.addNode(10);
+        dl_List.addNode(20);
+        dl_List.addNode(30);
+        dl_List.addNode(40);
+        dl_List.addNode(50);
+
+        //print the nodes of DoublyLinkedList
+        dl_List.printNodes();
+    }
+}
